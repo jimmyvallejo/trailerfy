039dc15e1afecdc83428000c65ace0a9c2438e9e
Jimmy <jrv1295@gmail.com>
2023-06-19 15:01:11 -0400

    added pages and navs

--- app/components/nav.js
+++ app/components/nav.js
@@ -24,20 +24,48 @@ const Nav = () => {
   },[session])
   
   
-  return <nav className="flex flex-row">
-      
-      {providers && Object.values(providers).map((provider) =>{
-        return  (
-        <button className="w-20 h-20 cursor-pointer" key={provider.name} onClick={() => signIn(provider.id)}>
-             {provider.name}
-        </button>
-        )
-      })}
-        <button className="ml-5" onClick={signOut}>
+  return (
+    <nav>
+      <div className="flex flex-row justify-between items-center border fixed w-screen">
+        <h1 className="text-2xl">Trailerfy</h1>
+        <div>
+          {providers &&
+            Object.values(providers).map((provider) => {
+              return (
+                <button
+                  className="w-20 h-20 cursor-pointer"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                >
+                  {provider.name}
+                </button>
+              );
+            })}
+          <button className="ml-5" onClick={signOut}>
             Sign out
-        </button>
-        {session && <h1 className="text-3xl">SESSION VERIFIED</h1>}
-  </nav>;
+          </button>
+          {session && <h1 className="text-3xl">SESSION VERIFIED</h1>}
+        </div>
+      </div>
+      <div className="flex flex-col border w-60 min-h-screen items-center justify-around fixed">
+        <Link className="mb-10 mt-20" href={"/"}>
+          Home
+        </Link>
+        <Link className="mb-10 mt-10" href={"/trending"}>
+          Trending
+        </Link>
+        <Link className="mb-10 mt-10" href={"/future"}>
+          Want to See
+        </Link>
+        <Link className="mb-10 mt-10" href={"/watched"}>
+          Watched
+        </Link>
+        <Link className="mb-10 mt-10" href={"/settings"}>
+          Settings
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
---
+++ app/trending/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Trending = () => {
+  return (
+    <div className='flex justify-center pt-40'>Trending</div>
+  )
+}
+
+export default Trending
